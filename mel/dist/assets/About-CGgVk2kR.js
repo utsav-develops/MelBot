762121111de536806a019524a74584a22c199050
dazu <utsavacharya96@gmail.com>
2026-03-11 18:40:27 +0545

    .

--- mel/dist/assets/About-CGgVk2kR.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-Cpa7DO53.js";function e(){return t.jsx(t.Fragment,{children:"about page"})}export{e as default};
--- mel/dist/assets/NotFound-DK3WfNAe.js
+++
@@ -1,1 +0,0 @@
-import{u as n,j as o}from"./index-Cpa7DO53.js";import{B as s}from"./button-B7-2LA8R.js";function i(){const t=n();return o.jsxs("div",{children:[o.jsx("h2",{children:"404 - Page not found"}),o.jsx(s,{onClick:()=>t(-1),children:"Go back"})]})}export{i as default};
