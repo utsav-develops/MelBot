523b97b9cc16d8fab0187b91d5c22fa2f67f3a55
dazu <utsavacharya96@gmail.com>
2026-03-11 15:21:03 +0545

    .

---
+++ mel/dist/assets/About-CSSXr8Wl.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-BclF7X9s.js";function e(){return t.jsx(t.Fragment,{children:"about page"})}export{e as default};
---
+++ mel/dist/assets/NotFound-OSq4TK0b.js
@@ -0,0 +1,1 @@
+import{u as n,j as o}from"./index-BclF7X9s.js";import{B as s}from"./button-CJceuB_4.js";function i(){const t=n();return o.jsxs("div",{children:[o.jsx("h2",{children:"404 - Page not found"}),o.jsx(s,{onClick:()=>t(-1),children:"Go back"})]})}export{i as default};
