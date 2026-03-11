1abb85f2e249f27e339cb573ff51dc3897dbbf02
dazu <utsavacharya96@gmail.com>
2026-03-11 18:10:18 +0545

    .

---
+++ mel/dist/assets/About-BB_AG5YP.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-B27bom8n.js";function e(){return t.jsx(t.Fragment,{children:"about page"})}export{e as default};
---
+++ mel/dist/assets/NotFound-Dp2gOmCi.js
@@ -0,0 +1,1 @@
+import{u as n,j as o}from"./index-B27bom8n.js";import{B as s}from"./button-DzBotoVR.js";function i(){const t=n();return o.jsxs("div",{children:[o.jsx("h2",{children:"404 - Page not found"}),o.jsx(s,{onClick:()=>t(-1),children:"Go back"})]})}export{i as default};
